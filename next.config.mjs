690c402db45a624b82bfd09733eec9d9a2f64479
Sharif Alalem <sharif.alalem@savings-united.com>
2024-08-07 02:51:02 +0200

    restructure styles folder

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const nextConfig = {
     sassOptions: {
-        additionalData: `@import "${path.resolve(__dirname, 'src/app/styles/variables.scss')}";`,
+        additionalData: `@import "${path.resolve(__dirname, 'src/styles/variables.scss')}";`,
         includePaths: [path.join(__dirname, 'styles')],
     },
 };
