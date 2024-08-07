f3819ab1d45969674f44191bac84474838258752
Sharif Alalem <sharif.alalem@savings-united.com>
2024-08-08 01:47:44 +0200

    implement baseCard comp. & add it to layout

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,13 +1,24 @@
 "use client"
 import styles from './page.module.scss';
+import { useProgressSteps } from '@/context/stepsProgress';
+import { useComponentsState } from '@/context/stepsComponents';
 import ProgressBar from '@/components/Progress/progress';
-import { useProgressSteps } from '@/context/steps';
+import BaseCard from '@/components/ui/BaseCard/baseCard';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
     const {steps,setSteps} = useProgressSteps();
+    const {components,setComponents} = useComponentsState();
+    const [currentIndex,setCurrentIndex] = useState(0);
+    useEffect(()=>{
+        setCurrentIndex(components.findIndex((component)=>component.active === true));
+    },[components])
     return ( 
         <main className={styles.main}> 
             <ProgressBar steps={steps} setSteps={setSteps} />
+            <BaseCard currentIndex={currentIndex} components={components} setComponents={setComponents}>
+                <h1>{components[currentIndex].id}</h1>
+            </BaseCard>
         </main>
      );
 }
